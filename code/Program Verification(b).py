23a854fc89b0d3579eb487d8e01ee92bdba91c28
soapppX <605071654@qq.com>
2025-10-22 01:03:05 +0200

    Update Program Verification(b).py

--- code/Program Verification(b).py
+++ code/Program Verification(b).py
@@ -2,25 +2,34 @@ from z3 import *
 import time
 
 start = time.time()
+
+
 x0 = BitVec('x0', 32)
-
-
 x = x0
 y = BitVecVal(1, 32)
 
 
+xlist = [x]
+ylist = [y]
 
 for i in range(15):
-    x= x + y
+    x = x + y
     y = 2 * y + 1
-
+    xlist.append(x)
+    ylist.append(y)
 
 s = Solver()
-s.add(x < x0 )
+
+
+s.add(ULT(xlist[14], BitVecVal(10**9, 32)))   
+s.add(UGE(xlist[15], BitVecVal(10**9, 32)))   
+
+
+s.add(ULT(x, x0))  
 
 if s.check() == sat:
     m = s.model()
-    print(m)
+    print("Overflow detected:", m)
 else:
     print("No overflow or underflow within 15 iterations.")
 
